4064b227c8ef945a210ef0e4f8129e5769f55fba
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-22 15:11:09 -0400

    add sesstion file in database

--- newsscraper/database/sesstion.py
+++ newsscraper/database/sesstion.py
@@ -1,13 +1,15 @@
 from typing import Generic
 
-from backend.core.config import settings
+from webscraping.newsscraper.core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is ", SQLALCHEMY_DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 # if you don't want to install postgres or any database, use sqlite, a file system based database,
 # uncomment below lines if you would like to use sqlite and comment above 2 lines of SQLALCHEMY_DATABASE_URL AND engine
