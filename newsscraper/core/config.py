be79c0e26959b14b914b6ddf8949e2aa4c25451a
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-22 15:54:54 -0400

    add providr url in .env

--- newsscraper/core/config.py
+++ newsscraper/core/config.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL")
+    RAJA_URL = os.getenv("RAJA_URL")
+    ASRIRAN_URL = os.getenv("ASRIRAN_URL")
 
 
 settings = Settings()
---
+++ newsscraper/main.py
@@ -0,0 +1,10 @@
+from webscraping.newsscraper.provider.rajanewsprovider import RajanewsProvider
+from webscraping.newsscraper.provider.asriranprovider import AsriranProvider
+from webscraping.newsscraper.database.repasitory.article import add_articles
+from webscraping.newsscraper.database.repasitory.provider import add_provider
+
+def run()
+    raja
+
+if __name__ == "__main__":
+    run()
