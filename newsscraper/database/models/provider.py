58f81c933e036b7e43ffc9fca508f1a29f4a771f
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-24 12:48:52 +0330

    delet database for maigrate into jango app

--- newsscraper/database/models/provider.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from ..base_class import Base
-
-
-class Providers(Base):
-    pro_id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    articles = relationship("Articles", back_populates="provider")
