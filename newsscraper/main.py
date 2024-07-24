bf3eedd60be2b7a88605cc1e0c78afabad1feed7
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-24 17:39:47 +0330

    modifi main for test scheduler module

--- newsscraper/main.py
+++ newsscraper/main.py
@@ -1,26 +1,38 @@
 from newsscraper.provider.asriranprovider import AsriranProvider
 from newsscraper.provider.rajanewsprovider import RajanewsProvider
 from newsscraper.core.config import settings
-from newsscraper.logs.logs_conf import logger
+from newsscraper.scheduling.scheduler import scheduler
+from newsscraper.scheduling.jobs.rajaprovider import rajaprovider_job
+from newsscraper.scheduling.jobs.asriranprovider import asriranprovider_job
 
 
-def run(massage="wellcome to web scraping app :)"):
+def rajascraper(url):
+    raja = RajanewsProvider(url)
+    return raja.scrape
+
+
+def asriranscraper(url):
+    asri = AsriranProvider(url)
+    return asri.scrape
+
+
+def scheduling(scrapers):
+    rajaprovider_job(scheduler, scrapers[0])
+    asriranprovider_job(scheduler, scrapers[1])
+
+
+def run(massage="Welcome to NewsScraper!"):
     print(massage)
-
-    raja = RajanewsProvider(settings.RAJA_URL)
-    asriran = AsriranProvider(settings.ASRIRAN_URL)
-
-    print(f"start scrape {settings.RAJA_URL} ...")
-    logger.info(f"start scrape {settings.RAJA_URL} ...")
-    rajanews = raja.scrape()
-    print(f"end scraping {settings.RAJA_URL}.")
-    logger.info(f"end scraping {settings.RAJA_URL}.")
-    print(f"start scrape {settings.ASRIRAN_URL}...")
-    logger.info(f"start scrape {settings.ASRIRAN_URL}...")
-    asrirannews = asriran.scrape()
-    print(f"end scraping {settings.ASRIRAN_URL}.")
-    logger.info(f"end scraping {settings.ASRIRAN_URL}.")
+    rajaprovider = rajascraper(settings.RAJA_URL)
+    asriranprovider = asriranscraper(settings.ASRIRAN_URL)
+    scheduling([rajaprovider, asriranprovider])
+    try:
+        print("Schedule Started ...")
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        print("Schedule Stopped ...")
+        pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
