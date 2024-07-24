ebe4bcac4545b2e423b392a7ed17369d802c752f
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-24 15:30:29 -0400

    add , add_provider in repository fodder in module db for create new provider in database

--- newsscraper/db/repository/provider.py
+++ newsscraper/db/repository/provider.py
@@ -5,3 +5,9 @@ from sqlalchemy.orm import Session
 def get_provider(name: str, session: Session):
     provider = session.query(Provider).filter_by(name=name).first()
     return provider
+
+
+def add_provider(name: str, session: Session):
+    provider = Provider(name)
+    session.add(provider)
+    session.commit()
